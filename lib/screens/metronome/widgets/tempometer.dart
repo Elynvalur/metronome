2a3ba6866d7a6ab9d8606eb5ba2c6b0b5eb9b8aa
Daniel <danielhettich96@gmail.com>
2022-09-28 15:49:29 +0200

    Change tempo controls

--- lib/screens/metronome/widgets/tempometer.dart
+++ lib/screens/metronome/widgets/tempometer.dart
@@ -9,19 +9,23 @@ class Tempometer extends StatelessWidget {
   final TimeSignature signature;
   final Function notifyParent;
 
-  Tempometer({super.key, required this.tempo, required this.signature, required this.notifyParent});
+  Tempometer(
+      {super.key,
+      required this.tempo,
+      required this.signature,
+      required this.notifyParent});
 
-  final TextStyle textStyleTimeSignature =  const TextStyle(
-      fontFamily: 'Times',
-      color: textPrimary,
-      fontWeight: FontWeight.bold,
-      fontSize: 25,
-    );
+  final TextStyle textStyleTimeSignature = const TextStyle(
+    fontFamily: 'Times',
+    color: textPrimary,
+    fontWeight: FontWeight.bold,
+    fontSize: 25,
+  );
 
   final ButtonStyle buttonStyle = ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(Colors.transparent),
-        elevation: MaterialStateProperty.all(10),
-        side: MaterialStateProperty.all(const BorderSide(color: textPrimary)));
+      backgroundColor: MaterialStateProperty.all(Colors.transparent),
+      elevation: MaterialStateProperty.all(10),
+      side: MaterialStateProperty.all(const BorderSide(color: textPrimary)));
 
   @override
   Widget build(BuildContext context) {
@@ -29,45 +33,56 @@ class Tempometer extends StatelessWidget {
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          
           children: [
-            ElevatedButton(
-              onPressed: (){
-                tempo.tempo--;
-                notifyParent();
-              },
-              style: buttonStyle, 
-              child: const Icon(Icons.remove)),
+            GestureDetector(
+                onTap: () {
+                  tempo.tempo--;
+                  notifyParent();
+                },
+                child: Container(
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                            style: BorderStyle.solid,
+                            color: textPrimary,
+                            strokeAlign: StrokeAlign.center)),
+                    child: const Icon(Icons.remove, size: 45, color: textPrimary))),
             const SizedBox(width: 20),
             Text(
               '${tempo.tempo}',
               style: const TextStyle(
-                  fontWeight: FontWeight.bold, color: textPrimary, fontSize: 80),
+                  fontWeight: FontWeight.bold,
+                  color: textPrimary,
+                  fontSize: 80),
             ),
             const SizedBox(width: 10),
-            Column(
-              children: [
-                Text('${signature.beatCount}', style: textStyleTimeSignature),
-                Container(
+            Column(children: [
+              Text('${signature.beatCount}', style: textStyleTimeSignature),
+              Container(
                   margin: const EdgeInsets.only(top: 5, bottom: 5),
                   decoration: const BoxDecoration(color: textPrimary),
                   child: const SizedBox(width: 25, height: 2)),
-                Text('${signature.signature}', style: textStyleTimeSignature),
-              ]
-            ),
+              Text('${signature.signature}', style: textStyleTimeSignature),
+            ]),
             const SizedBox(width: 20),
-            ElevatedButton(onPressed: (){
-                tempo.tempo++;
-                notifyParent();
-              },
-              style: buttonStyle,
-              child: const Icon(Icons.add)),
+            GestureDetector(
+                onTap: () {
+                  tempo.tempo++;
+                  notifyParent();
+                },
+                child: Container(
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                            style: BorderStyle.solid,
+                            color: textPrimary,
+                            strokeAlign: StrokeAlign.center)),
+                    child: const Icon(Icons.add, size: 45, color: textPrimary)))
           ],
         ),
         Text(
           'One beat every ${(tempo.interval).toInt()} ms.',
-          style: const TextStyle(
-            color: textPrimary),
+          style: const TextStyle(color: textPrimary),
         ),
       ],
     );
