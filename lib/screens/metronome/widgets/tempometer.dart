511bc70575ff90e9d2a3c9e49bda0963ce544274
Daniel <danielhettich96@gmail.com>
2022-10-04 22:54:52 +0200

    Implement HoldDetector for change-tempo-buttons.

--- lib/screens/metronome/widgets/tempometer.dart
+++ lib/screens/metronome/widgets/tempometer.dart
@@ -1,7 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:metronome/models/time_signature.dart';
 import '../../../models/tempo.dart';
-
+import 'package:holding_gesture/holding_gesture.dart';
 import '../../../constants/colors.dart';
 
 class Tempometer extends StatelessWidget {
@@ -9,93 +11,103 @@ class Tempometer extends StatelessWidget {
   final TimeSignature signature;
   final Function notifyParent;
 
-  Tempometer(
+  const Tempometer(
       {super.key,
       required this.tempo,
       required this.signature,
       required this.notifyParent});
 
-  final TextStyle textStyleTimeSignature = const TextStyle(
-    fontFamily: 'Times',
-    color: textPrimary,
-    fontWeight: FontWeight.bold,
-    fontSize: 25,
-  );
-
-  final ButtonStyle buttonStyle = ButtonStyle(
-      backgroundColor: MaterialStateProperty.all(Colors.transparent),
-      elevation: MaterialStateProperty.all(10),
-      side: MaterialStateProperty.all(const BorderSide(color: textPrimary)));
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _buildChangeTempoButton(icon: Icons.remove, onTap: () => tempo.tempo--),
-            const SizedBox(width: 20),
-            // Tempo
-            _buildTempoIndicator(),
-
-            const SizedBox(width: 10),
-            
-            _buildTimeSignatureIndicator(),
-
-            const SizedBox(width: 20),
-
-            _buildChangeTempoButton(icon: Icons.add, onTap: () => tempo.tempo++),
-          ],
-        ),
-        Text(
-          'One beat every ${(tempo.interval).toInt()} ms.',
-          style: const TextStyle(color: textPrimary),
-        ),
-      ],
+    return Container(
+      decoration: const BoxDecoration(color: Color.fromARGB(75, 0, 0, 0)),
+      padding: const EdgeInsets.only(top: 20, bottom: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+              Container(
+                width: 60,
+                //decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+                child: _buildChangeTempoButton(icon: Icons.remove, onHold: () => tempo.tempo--)),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    _buildTempoIndicator(),
+                    const SizedBox(width: 10),
+                    _buildTimeSignatureIndicator(),
+                  ],
+                ),
+              ),
+              Container(
+                width: 60,
+                //decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+                child: _buildChangeTempoButton(icon: Icons.add, onHold: () => tempo.tempo++))
+              
+            ],
+          ),
+          Text(
+            'One beat every ${(tempo.interval).toInt()} ms.',
+            style: const TextStyle(color: textPrimary),
+          ),
+        ],
+      ),
     );
   }
 
-  Widget _buildChangeTempoButton({IconData? icon, required Function onTap}){
-
-    return GestureDetector(
-                onTap: () {
-                  onTap();
-                  notifyParent();
-                },
-                child: Container(
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                            style: BorderStyle.solid,
-                            color: textPrimary,
-                            strokeAlign: BorderSide.strokeAlignCenter)),
-                    child: Icon(icon,
-                        size: 45, color: textPrimary)));
-
+  Widget _buildChangeTempoButton({IconData? icon, required Function onHold}) {
+    return HoldDetector(
+        onHold: () {
+          onHold();
+          notifyParent();
+        },
+        holdTimeout: const Duration(milliseconds: 80),
+        enableHapticFeedback: true,
+        child: Container(
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                    style: BorderStyle.solid,
+                    color: textPrimary,
+                    strokeAlign: BorderSide.strokeAlignCenter)),
+            child: Icon(icon, size: 45, color: textPrimary)));
   }
 
-  Widget _buildTempoIndicator(){
-
+  Widget _buildTempoIndicator() {
     const TextStyle textStyleTempoIndicator = TextStyle(
-    fontWeight: FontWeight.bold, color: textPrimary, fontSize: 80);
+        fontWeight: FontWeight.bold,
+        color: textPrimary,
+        fontSize: 80,
+        fontFeatures: [FontFeature.tabularFigures()],
+        );
 
     return GestureDetector(
-      onLongPress: () {
-        tempo.tempo++;
-        notifyParent();
-      },
-      child: Text('${tempo.tempo}', style: textStyleTempoIndicator));
+        onTap: () {
+          tempo.tempo++;
+          notifyParent();
+        },
+        child: Text('${tempo.tempo}', style: textStyleTempoIndicator));
   }
 
-  Widget _buildTimeSignatureIndicator(){
+  Widget _buildTimeSignatureIndicator() {
+    const TextStyle textStyleTimeSignature = TextStyle(
+      fontFamily: 'Times',
+      color: textPrimary,
+      fontWeight: FontWeight.bold,
+      fontSize: 25,
+      fontFeatures: [FontFeature.tabularFigures()],
+    );
+
     return Column(children: [
-              Text('${signature.beatCount}', style: textStyleTimeSignature),
-              Container(
-                  margin: const EdgeInsets.only(top: 5, bottom: 5),
-                  decoration: const BoxDecoration(color: textPrimary),
-                  child: const SizedBox(width: 25, height: 2)),
-              Text('${signature.signature}', style: textStyleTimeSignature),
-            ]);
+      Text('${signature.beatCount}', style: textStyleTimeSignature),
+      Container(
+          margin: const EdgeInsets.only(top: 5, bottom: 5),
+          decoration: const BoxDecoration(color: textPrimary),
+          child: const SizedBox(width: 25, height: 2)),
+      Text('${signature.signature}', style: textStyleTimeSignature),
+    ]);
   }
 }
